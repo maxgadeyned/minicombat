468771bfe2806b2ddf06e905e058ba280a9956af
Maxim <max.gadeyne.d@gmail.com>
2026-03-07 19:24:05 +0100

    Update minicombat

--- server/signaling.js
+++ server/signaling.js
@@ -51,7 +51,7 @@ function cleanupRoom(code, reason) {
 }
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
   res.end("Miniature Combat server – connect via WebSocket (wss://)");
 });
 const wss = new WebSocketServer({ server });
